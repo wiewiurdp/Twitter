bdbc8198244dd5c397c345bcd0210e32de20c20e
Mateusz Kolankowski <matt@10g.pl>
2017-04-23 14:58:53 +0200

    connection added

---
+++ public/connection.php
@@ -0,0 +1,12 @@
+
+<?php
+
+include_once '../config/db.php';
+
+//Poniżej napisz kod łączący się z bazą danych
+$connection = new PDO(sprintf("mysql:host=%s;dbname=%s", $hostname, $dbname), $user, $password);
+
+if ($connection->errorCode() != null) {
+    var_dump($connection->errorInfo());
+    die();
+}
