34188f42767c294044ce8dff923fb8be7c490f14
Mateusz Kolankowski <matt@10g.pl>
2017-05-24 17:20:49 +0200

    reply to the message functionality added

--- public/admin/newMessage.php
+++ public/admin/newMessage.php
@@ -22,22 +22,48 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['messageText'])) {
 
 <html>
 <body>
-<h1>Napisz wiadomość</h1>
+<h1><?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['messageId'])){
+        echo 'Odpowiedz na wiadomość';
+    } else {
+        echo 'Napisz wiadomość';
+    }
+    ?>
+    </h1>
 <form method="post">
-   Do: <br>
+    Do: <br>
     <select name="receiverId">
         <?php
-        for ($i = 0; $i < count($allUsers); $i++) {
-            $user = $allUsers[$i]->getUsername();
-            $id = $allUsers[$i]->getId();
-            echo "<option value='$id'>$user</option>";
+        if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['messageId'])) {
+            $message = Message::loadMessageById($connection,$_POST['messageId']);
+            $senderId = $message->getSenderId();
+            $topic = $message->getTopic();
+            $text = $message->getText();
+            $senderUser = User::loadUserById($connection, $senderId);
+            $senderUsername = $senderUser->getUsername();
+            echo "<option value='$senderId'>$senderUsername</option>";
+        } else {
+            for ($i = 0; $i < count($allUsers); $i++) {
+                $user = $allUsers[$i]->getUsername();
+                $id = $allUsers[$i]->getId();
+                echo "<option value='$id'>$user</option>";
+            }
         }
         ?>
     </select>
     <br>
-    Temat: <input name="topic">
+    Temat: <input name="topic" value="<?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['messageId'])){
+        echo "Re: ".$topic;
+    }
+    ?>
+">
     <br>
-    <br><textarea name="messageText" cols="60" rows="5" placeholder="O czym chcesz napisać?"></textarea>
+    <br><textarea name="messageText" cols="60" rows="5"><?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['messageId'])){
+            echo $text;
+        }
+        ?></textarea>
     <br>
     <button type="submit">Wyślij</button>
     <br>
--- public/admin/showMessage.php
+++ public/admin/showMessage.php
@@ -2,7 +2,12 @@
 
 include_once __DIR__ . '/../bootstrap.php';
 
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['messageId'])) {
+    include_once __DIR__ . '/newMessage.php';
+}
+
 $message = Message::loadMessageById($connection, $_GET['messageId']);
+$messageId = $_GET['messageId'];
 $receiverId = $message->getReceiverId();
 $senderId = $message->getSenderId();
 $user = User::loadUserById($connection, $senderId);
@@ -13,8 +18,10 @@ $creationDate = $message->getCreationDate();
 if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['messageId']) && ($_SESSION['id'] == $senderId || $_SESSION['id'] == $receiverId)) {
     $message->setReadCheck(0);
     $message->save($connection);
-    echo (sprintf("<b>Od: %s</b><h2>Temat: %s</h2><br>%s<br><br><code>%s</code>",$username,$topic, $text, $creationDate));
+    echo(sprintf("<b>Od: %s</b><h2>Temat: %s</h2><br>%s<br><br><code>%s</code><br><br> <form method='post' action=''><button type='submit' name='messageId' value='$messageId'>Odpowiedz</button></form>", $username, $topic, $text, $creationDate));
 
-} else {
-   echo "Nie możesz czytac nie swoich wiadomości!";
+} elseif ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['messageId']) && ($_SESSION['id'] != $senderId || $_SESSION['id'] != $receiverId)) {
+    echo "Nie możesz czytac nie swoich wiadomości!";
 }
+?>
+
